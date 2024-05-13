8675db3d105502e50c881b032cd08cfc96433913
1DanielSolar <danielsolar2003@gmail.com>
2024-05-12 22:14:28 -0700

    Update users.js
    updated page working on

--- js/users.js
+++ js/users.js
@@ -249,7 +249,7 @@ let students = [
     imageurl: "https://fakeimg.pl/600x400?text=Not+Defined",
     gender: "male",
     xp: 0,
-    currentlyWorkingOn: 41
+    currentlyWorkingOn: 51
   },
   {
     name: "Andrea Ceballos",
