2ef58885355215ede8001ccffcb38ef39bb8452d
Remsey Mailjard <remsey@skills4-it.nl>
2024-05-14 13:12:08 -0500

    Updates js

--- js/users.js
+++ js/users.js
@@ -6,9 +6,9 @@ let students = [
     linkedin: "https://www.linkedin.com/in/sabrina-robles-231112170/",
     email: "sabrinatrobles22@gmail.com",
     nickname: "Sabri",
-    imageurl:
+    imageUrl:
       "https://ih1.redbubble.net/image.3753796206.6697/st,small,507x507-pad,600x600,f8f8f8.jpg",
-    imageURLLinkedin:
+    imageUrlLinkedin:
       "https://media.licdn.com/dms/image/D5603AQFre02VzeThcA/profile-displayphoto-shrink_400_400/0/1711770747511?e=1720656000&v=beta&t=rwxNPYA9tZNxr735lItRNDmKenGq7ho06bQcRNLcSSg",
     gender: "female",
     devices: "windows",
@@ -22,9 +22,10 @@ let students = [
     linkedin: "https://www.linkedin.com/in/brett-spokes-8b758b29b/",
     email: "bspokes@appdev.yearup.org",
     nickname: "Any Key",
-    imageurl:
+    imageUrl:
       "https://media.licdn.com/dms/image/D4E03AQHsvMOXofd4eg/profile-displayphoto-shrink_800_800/0/1701278215042?e=1720656000&v=beta&t=W-I_I4aKUyVjcXNKc0lfkrP7ftWPD-fACwEE1NqLUlw",
     gender: "male",
+    devices: "Windows",
     xp: 0,
     currentlyWorkingOn: 75,
   },
@@ -35,13 +36,8 @@ let students = [
     linkedin: "https://www.linkedin.com/in/alfi-ali-015506225/",
     email: "asamatar@appdev.yearup.org",
     nickname: "luffy",
-<<<<<<< AlfiAli
-    imageurl: "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2022/12/luffy-with-his-straw-hat.jpg",
-    pagURL :"alfi.html",
-=======
-    imageurl:
+    imageUrl:
       "https://media.licdn.com/dms/image/C4E03AQEoG-gPExm-cw/profile-displayphoto-shrink_400_400/0/1636537401196?e=1720656000&v=beta&t=nfp6t_VXyLbYVvmFFXz2ct3Mx8V1P8Xm9qzrJQGOVMc",
->>>>>>> main
     gender: "male",
     devices: "Windows",
     xp: 0,
@@ -54,7 +50,7 @@ let students = [
     linkedin: "https://www.linkedin.com/in/abdikadir-samatar/",
     email: "asamatar@appdev.yearup.org",
     nickname: "deer",
-    imageurl:
+    imageUrl:
       "https://media.licdn.com/dms/image/D4E03AQHCIfkZE-wCMA/profile-displayphoto-shrink_200_200/0/1710697206134?e=1720656000&v=beta&t=UlWXGrpsVIy4L_BTQgve95zK0MHqXjX2mvQDN721Nfo",
     gender: "male",
     devices: "MAC",
@@ -68,7 +64,7 @@ let students = [
     linkedin: "https://www.linkedin.com/in/dante-angerame-739899192/",
     email: "dangerame@appdev.yearup.org",
     nickname: "The Woodchuck",
-    imageurl: "https://a-z-animals.com/media/2021/10/groundhog-1024x535.jpg",
+    imageUrl: "https://a-z-animals.com/media/2021/10/groundhog-1024x535.jpg",
     gender: "male",
     xp: 0,
     currentlyWorkingOn: 42,
@@ -81,7 +77,7 @@ let students = [
       "https://www.linkedin.com/in/guadalupe-argumedo-saucedo-2b63b8303/",
     email: "gargumedosaucedo@appdev.yearup.org",
     nickname: "CodeLupe",
-    imageurl:
+    imageUrl:
       "https://fakeimg.pl/600x400/7b37c4/fff700?text=Lupe+Argumedo&font=bebas",
     gender: "female",
     devices: "MAC",
@@ -95,10 +91,10 @@ let students = [
     linkedin: "https://www.linkedin.com/in/jaranesca-braker-227810253/",
     email: "jbraker@appdev.yearup.org",
     nickname: "JSJ (JavaScript Jaranesca)",
-    imageurl: "https://fakeimg.pl/600x400/ffe44c/db29db?text=Jaranesca+Braker",
+    imageUrl: "https://fakeimg.pl/600x400/ffe44c/db29db?text=Jaranesca+Braker",
     gender: "female",
     xp: 0,
-    currentlyWorkingOn: "55",
+    currentlyWorkingOn: 55,
   },
   {
     name: "Jesús Mimila",
@@ -107,7 +103,7 @@ let students = [
     linkedin: "https://www.linkedin.com/in/jesus-mimils-435b32274/",
     email: "jmimila@appdev.yearup.org",
     nickname: "JJ",
-    imageurl:
+    imageUrl:
       "https://media.licdn.com/dms/image/D4E03AQFN9jSMTdbzDA/profile-displayphoto-shrink_400_400/0/1715103915979?e=1720656000&v=beta&t=5QnIvkUZJ9RX9KlBJbEPdKgv95eNmlrGAiDgvYd9k8A",
     devices: "MAC",
     gender: "male",
@@ -122,13 +118,12 @@ let students = [
     linkedin: "https://www.linkedin.com/in/kaaliyahgray-b03281237",
     email: "kgray@appdev.yearup.org",
     nickname: "leetheMrE",
-    imageurl:
+    imageUrl:
       "https://media.licdn.com/dms/image/D5603AQEJtVL8lJLJpw/profile-displayphoto-shrink_400_400/0/1711738921148?e=1720656000&v=beta&t=VbkGPpHEA5sBDc8C96HVHrmvtBEJdt5TWW2bV-zuy18",
     gender: "female",
     xp: 0,
-
-    currentlyWorkingOn: "Page 59",
-    personalWebpage: "http://127.0.0.1:5500/kaaliyah.html"
+    currentlyWorkingOn: 59,
+    personalWebpage: "http://127.0.0.1:5500/kaaliyah.html",
   },
   {
     name: "Kadiatou Barry (Mac)",
@@ -137,7 +132,7 @@ let students = [
     linkedin: "https://www.linkedin.com/in/kadiatou-barry-a57bb8235/",
     email: "kbarry@appdev.yearup.org",
     nickname: "the connoisseur",
-    imageurl:
+    imageUrl:
       "https://media.licdn.com/dms/image/C4E03AQELUNIRkpL2mQ/profile-displayphoto-shrink_400_400/0/1650085102341?e=1720656000&v=beta&t=mmwdGPGrsMGSixWu1_5aNj-9Cefa_n3CuCY3XSLXIr4",
     gender: "female",
     xp: 0,
@@ -150,11 +145,11 @@ let students = [
     linkedin: "https://www.linkedin.com/in/kehanifen",
     email: "khanifen@appdev.yearup.org",
     nickname: "Agent K",
-    imageurl:
+    imageUrl:
       "https://media.licdn.com/dms/image/D5603AQFHQ46apuKgbg/profile-displayphoto-shrink_400_400/0/1690255441190?e=1720656000&v=beta&t=vfjPfEJcHiS51C1tlT9DOG76R5n_PPJHoKirGzlHEuU",
     gender: "female",
     xp: 0,
-    currentlyWorkingOn: "Page nr",
+    currentlyWorkingOn: 0,
   },
   {
     name: "Leslie Alvarado (Mac)",
@@ -163,12 +158,12 @@ let students = [
     linkedin: "https://www.linkedin.com/in/leslie-alvarado-connect/",
     email: "lalvarado@appdev.yearup.org",
     nickname: "The ScriptSiren",
-    imageurl:
+    imageUrl:
       "https://media.licdn.com/dms/image/D5603AQHD4XcsiS9_7Q/profile-displayphoto-shrink_400_400/0/1714775792411?e=1720656000&v=beta&t=ti7w2I4510-NOGoVQqClRjXZ-XGsu673mSn6JDWZtRY",
     gender: "female",
     xp: 0,
     currentlyWorkingOn: 48,
-    page: "/leslie.html"
+    personalWebpage: "/leslie.html",
   },
   {
     name: "Maximo Mayo",
@@ -177,11 +172,11 @@ let students = [
     linkedin: "https://www.linkedin.com/in/maximo-mayo-a2ba10252/",
     email: "mmayo@appdev.yearup.org",
     nickname: "reference error",
-    imageurl:
+    imageUrl:
       "https://media.licdn.com/dms/image/D5635AQGJEoOx7l2_wg/profile-framedphoto-shrink_400_400/0/1668459290344?e=1715983200&v=beta&t=HUDrgjEalAYADICjvMh3t91vcTis3VBKVAUhR9FSgRk",
     gender: "male",
     xp: 0,
-    currentlyWorkingOn: "Page nr",
+    currentlyWorkingOn: 0,
   },
   {
     name: "Moncerrat Lua Salas",
@@ -190,7 +185,7 @@ let students = [
     linkedin: "https://www.linkedin.com/in/moncerrat-lua-salas/",
     email: "mluasalas@appdev.yearup.org",
     nickname: "Monce",
-    imageurl: "https://fakeimg.pl/600x400/fc8dd7/fad2e8?text=monce+so+swag",
+    imageUrl: "https://fakeimg.pl/600x400/fc8dd7/fad2e8?text=monce+so+swag",
     gender: "female",
     xp: 0,
     currentlyWorkingOn: 32,
@@ -200,13 +195,14 @@ let students = [
     group: "App Dev Team",
     github: "https://github.com/NiranAbdulqader",
     linkedin: "https://www.linkedin.com/in/niran-abdulqader-/",
-    Website:"Niran.html",
+    personalWebpage: "Niran.html",
     email: "nabdulqader@appdev.yearup.org",
     nickname: "Techie Diva",
-    imageurl: "https://media.licdn.com/dms/image/C5603AQHCS9pI_VLNZQ/profile-displayphoto-shrink_800_800/0/1634626091348?e=1720656000&v=beta&t=GgD0cMSHbOwXtLXvKnhmQFg57BFgomFovZXPY28b14Q",
+    imageUrl:
+      "https://media.licdn.com/dms/image/C5603AQHCS9pI_VLNZQ/profile-displayphoto-shrink_800_800/0/1634626091348?e=1720656000&v=beta&t=GgD0cMSHbOwXtLXvKnhmQFg57BFgomFovZXPY28b14Q",
     gender: "female",
     xp: 0,
-    currentlyWorkingOn: 48
+    currentlyWorkingOn: 48,
   },
   {
     name: "Rene Banuelos",
@@ -215,9 +211,9 @@ let students = [
     linkedin: "https://www.linkedin.com/in/rene-banuelos-1881b52a2",
     email: "rbanuelos@appdev.yearup.org",
     nickname: "Cyber Samurai",
-    imageurl: "https://fakeimg.pl/400x400/120707/40b2eb?text=Rene+B",
+    imageUrl: "https://fakeimg.pl/400x400/120707/40b2eb?text=Rene+B",
     gender: "male",
-    web: "rene.html",
+    personalWebpage: "rene.html",
     xp: 0,
     currentlyWorkingOn: 32,
   },
@@ -228,7 +224,7 @@ let students = [
     linkedin: "https://www.linkedin.com/in/uriel-delgado-3a49622bb/",
     email: "udelgado@appdev.yearup.org",
     nickname: "YaBoiUri",
-    imageurl:
+    imageUrl:
       "https://fakeimg.pl/600x400/2ac458/c942c7?text=This+is+Uriel's+image.",
     gender: "male",
     xp: 0,
@@ -241,7 +237,7 @@ let students = [
     linkedin: "https://www.linkedin.com/in/victor-castaneda/",
     email: "vcastaneda@appdev.yearup.org",
     nickname: "vic.exe",
-    imageurl:
+    imageUrl:
       "https://media.licdn.com/dms/image/C4E03AQEzzEbWxGnohQ/profile-displayphoto-shrink_400_400/0/1559835011604?e=1720656000&v=beta&t=fSAI7saiB20o34fW1rmiBNC0vE_wkb9-i93ge6dKljs",
     gender: "male",
     xp: 0,
@@ -254,7 +250,7 @@ let students = [
     linkedin: "https://www.linkedin.com/in/remseymailjard/",
     email: "remsey@skills4-it.nl",
     nickname: "Mailjard the Magnificent",
-    imageurl: "https://www.remsey.nl/assets/images/remsey-mailjard.jpg",
+    imageUrl: "https://www.remsey.nl/assets/images/remsey-mailjard.jpg",
     gender: "male",
     xp: 0,
     currentlyWorkingOn: 0,
@@ -266,7 +262,7 @@ let students = [
     linkedin: "https://www.linkedin.com/in/daniel-solar-464357300/",
     email: "dsolar@appdev.yearup.org",
     nickname: "Not Defined",
-    imageurl:
+    imageUrl:
       "https://media.licdn.com/dms/image/D4D03AQHBE8cnwrNeZQ/profile-displayphoto-shrink_400_400/0/1715104324245?e=1720656000&v=beta&t=DIUF_40UXczsxkRaZB3Azjwsbg2TQgNuwk8da8-g9ek",
     gender: "male",
     xp: 0,
@@ -279,9 +275,9 @@ let students = [
     linkedin: "https://www.linkedin.com/in/andrea-irary/",
     email: "iraryc@gmail.com",
     nickname: "Kung Fu Koder",
-    imageurl:
+    imageUrl:
       "https://media.licdn.com/dms/image/D5635AQEGBhG_qU_xJw/profile-framedphoto-shrink_400_400/0/1711041596221?e=1715983200&v=beta&t=3_LMe7iPRKe-cq6GHvXFEgnWp9Q_5HiY2XaDrFg_3EM",
-    gender: "Female",
+    gender: "female",
     xp: 0,
     currentlyWorkingOn: 48,
   },
