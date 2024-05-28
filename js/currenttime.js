a3c3c20010298fed435630f64aa6c35319fc893c
Remsey Mailjard <remsey@skills4-it.nl>
2024-05-28 11:06:13 -0500

    Updated the timezone
    Updated the timezone

--- js/currenttime.js
+++ js/currenttime.js
@@ -1,10 +1,10 @@
 function updateClock() {
     var now = new Date();
     var phoenixTime = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours() - 7, now.getUTCMinutes(), now.getUTCSeconds());
-    var netherlandsTime = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours() + 2, now.getUTCMinutes(), now.getUTCSeconds());
+    var panamaTime = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours() - 5, now.getUTCMinutes(), now.getUTCSeconds());
 
     document.getElementById("phoenixTime").textContent = phoenixTime.toLocaleTimeString();
-    document.getElementById("netherlandsTime").textContent = netherlandsTime.toLocaleTimeString();
+    document.getElementById("panamaTime").textContent = panamaTime.toLocaleTimeString();
     
     // Adding the countdown display function
     updateCountdown(phoenixTime);
